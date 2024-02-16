ea1495b9b2feaa1369ad37884e350875107c3499
hhhhhq <Mr.j@tom.com>
2024-02-16 17:23:09 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import testPic from '/logo512.png';
 
 function App() {
+  // console.log(logo);
+  // console.log('public url: ', process.env);
+  // console.log('testPic:', testPic);
+  // console.dir(`${process.env.PUBLIC_URL}` + '/logo512.png');
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log('run code...');
+  }, [count])
+  function handleClick() {
+    console.log('click...');
+    setCount(count + 1)
+  }
   return (
     <div className="App">
+      {/* <img src="/logo512.png" alt="big logo test" /> */}
+      {/* <div className="pic-test" style={{ background: url('/logo512.png'), height: '200px' }}></div> */}
+      {/* <div className="pic-test" style={{ background: `url('${process.env.PUBLIC_URL}/logo512.png')`, height: '200px' }}></div> */}
+      {/* <img src={process.env.PUBLIC_URL + '/logo512.png'} alt="" /> */}
+      {/* <img src="./logo.svg" alt="" /> // 这样写路径会原样编译到结果中 */}
+      {/* <div className="pic-test"></div> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={handleClick}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
